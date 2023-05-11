98b8174aabed3cf97f0949eced92c005e3345e84
Luke <55098790+knotimpressed@users.noreply.github.com>
2023-05-11 13:41:31 -0400

    changed default speed change

--- tools.js
+++ tools.js
@@ -2,4 +2,4 @@
 var elements = document.getElementsByTagName("*");
 for(var id = 0; id < elements.length; ++id) { elements[id].oncontextmenu = null; }
 // set video speed:
-document.querySelector('video').playbackRate = 3.0;
+document.querySelector('video').playbackRate = 2.8;
