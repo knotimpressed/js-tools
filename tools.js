539a10fb456db3cbb73983563b9b0f6b6c53b01b
Luke <55098790+knotimpressed@users.noreply.github.com>
2023-05-11 10:34:43 -0400

    vscode.dev is sick!

---
+++ tools.js
@@ -0,0 +1,5 @@
+// basic allow right click (paste into console):
+var elements = document.getElementsByTagName("*");
+for(var id = 0; id < elements.length; ++id) { elements[id].oncontextmenu = null; }
+// set video speed:
+document.querySelector('video').playbackRate = 3.0;
